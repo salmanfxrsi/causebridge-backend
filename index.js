c644a0c8a23910f94338333aecdeb96bb862c72d
salmanfxrsi <salmanxprivate@gmail.com>
2024-12-26 14:33:37 +0600

    feat: add endpoint to delete specific volunteer request by ID

--- index.js
+++ index.js
@@ -120,6 +120,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/delete-specific-request/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerRequestsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
