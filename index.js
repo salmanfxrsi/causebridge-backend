491033cffc14b54be659f637caa62aab852a6d59
salmanfxrsi <salmanxprivate@gmail.com>
2024-12-26 21:08:21 +0600

    feat: Implement JWT authentication and secure private routes with token verification

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 9000;
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://cause-bridge.web.app"],
+  credentials: true,
+  optionalSuccessStatus: 200,
+};
+
 // middleware
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // connect to mongodb
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://causeBridge:0oKUMEUL2axExtel@cluster0.upkox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upkox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,16 +29,55 @@ const client = new MongoClient(uri, {
   },
 });
 
-const volunteerNeededPostCollection = client
-  .db("causebridgeDB")
-  .collection("volunteerPOSTS");
-const volunteerRequestsCollection = client
-  .db("causebridgeDB")
-  .collection("volunteerRequests");
+// verify jwt token
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).send({ message: "unauthorized access" });
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+  });
+
+  next();
+};
 
 async function run() {
   try {
-    // await client.connect();
+    const volunteerNeededPostCollection = client
+      .db("causebridgeDB")
+      .collection("volunteerPOSTS");
+    const volunteerRequestsCollection = client
+      .db("causebridgeDB")
+      .collection("volunteerRequests");
+
+    // generate jwt
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      // create token
+      const token = jwt.sign(email, process.env.SECRET_KEY, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    // logout jwt from cookie
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // get all volunteer posts from db
     app.get("/volunteer-need-posts", async (req, res) => {
@@ -37,9 +85,9 @@ async function run() {
       let query = {
         postTitle: {
           $regex: search,
-          $options: 'i',
+          $options: "i",
         },
-      }
+      };
       const result = await volunteerNeededPostCollection.find(query).toArray();
       res.send(result);
     });
@@ -53,20 +101,30 @@ async function run() {
     });
 
     // get specific user posts using email
-    app.get("/get-specific-user-post/:email", async (req, res) => {
+    app.get("/get-specific-user-post/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
+      const decodedEmail = req.user?.email;
+      if (decodedEmail !== email)
+        return res.status(401).send({ message: "unauthorized access" });
       const query = { organizerEmail: email };
       const result = await volunteerNeededPostCollection.find(query).toArray();
       res.send(result);
     });
 
     // get specific user request to be a volunteer using email
-    app.get("/volunteer-request-posts/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { volunteerEmail: email };
-      const result = await volunteerRequestsCollection.find(query).toArray();
-      res.send(result);
-    });
+    app.get(
+      "/volunteer-request-posts/:email",
+      verifyToken,
+      async (req, res) => {
+        const email = req.params.email;
+        const decodedEmail = req.user?.email;
+        if (decodedEmail !== email)
+          return res.status(401).send({ message: "unauthorized access" });
+        const query = { volunteerEmail: email };
+        const result = await volunteerRequestsCollection.find(query).toArray();
+        res.send(result);
+      }
+    );
 
     // showcase volunteer need 6 posts with upcoming deadlines
     app.get("/upcoming-deadline-posts", async (req, res) => {
@@ -93,11 +151,12 @@ async function run() {
       const result = await volunteerRequestsCollection.insertOne(postData);
 
       // decrease volunteersNeeded number from main post
-      const filter = { _id: new ObjectId(postData.postId) }
+      const filter = { _id: new ObjectId(postData.postId) };
       const update = {
-        $inc: {volunteersNeeded: -1}
-      } 
-      const updateVolunteerNeededNumber = await volunteerNeededPostCollection.updateOne(filter, update)
+        $inc: { volunteersNeeded: -1 },
+      };
+      const updateVolunteerNeededNumber =
+        await volunteerNeededPostCollection.updateOne(filter, update);
 
       res.send(result);
     });
